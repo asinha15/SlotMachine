9dccb17d6e71b2e225346a536db04f36a366b9ad
Arnab Sinha <arisin@gmail.com>
2015-03-24 10:37:00 -0700

    Configuring containers

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Arnab Sinha on 3/23/15.
+//  Copyright (c) 2015 Arnab Sinha. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
